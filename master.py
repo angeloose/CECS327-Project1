7f3b3f6fba201385d60eda2e002f7bdf7f1027a8
Angelo <angelocervanaa@gmail.com>
2025-03-11 19:45:22 -0700

    Broadcast protocol automatically runs and works!!

--- master.py
+++ master.py
@@ -2,36 +2,44 @@ import socket
 import os
 import time
 
-cluster_a_ips = ["172.16.0.2", "172.16.0.3", "172.16.0.4", "172.16.0.5", "172.16.0.6", "172.16.0.7", "172.16.0.8", "172.16.0.9"]
-cluster_b_ips = ["172.16.0.10", "172.16.0.11", "172.16.0.12", "172.16.0.13", "172.16.0.14", "172.16.0.15", "172.16.0.16", "172.16.0.17"]
+cluster_a_ips = ["172.16.0.3", "172.16.0.4", "172.16.0.5", "172.16.0.6", "172.16.0.7", "172.16.0.8", "172.16.0.9"]
+cluster_b_ips = ["172.16.0.11", "172.16.0.12", "172.16.0.13", "172.16.0.14", "172.16.0.15", "172.16.0.16", "172.16.0.17"]
 
 ip_address = socket.gethostbyname(socket.gethostname())
 
 def start_server():
+    # Wait 2 seconds to ensure container nodes initiailize
+    time.sleep(2)
+
     print(f"{container_name} starting UDP server for receiving messages")
+
+    # Create UDP socket
     server = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    server.bind(("0.0.0.0", 9999))  # Bind to 0.0.0.0 which listens to all network interfaces
+
+    # Bind to 0.0.0.0 which listens to all network interfaces
+    server.bind(("0.0.0.0", 9999))  
 
     print(f"{container_name} Listening on port 9999...")
 
-    for _ in range(7):  # Run until a message is sent to each of the 7 other containers
-        while True:
-            data, addr = server.recvfrom(1024)
-            server_ip = addr[0]
-            if server_ip in allowed_ips:  # Ensure master only finds ips within its cluster
-                print(data.decode())
-                server.sendto(f"Hello from {container_name}".encode(), addr)
-                break
-            time.sleep(1)
+    for ip in allowed_ips:  # Run until a message is sent to each of the 7 other containers
+
+        # Send message to ip listening to port 9999
+        server.sendto(f" Can you hear me my children. It is I, {container_name}".encode(), (ip, 9999))
+        data, addr = server.recvfrom(1024)
+
+        print(data.decode())
+        server.sendto(f"Hello from {container_name}".encode(), addr)
+
+    server.close()
 
 if __name__ == "__main__":
     container_name = os.getenv("container_name", "unknown")  # Get name of container
     print(f"Hello I am {container_name}")
 
+    print(container_name[8])
     if container_name[8] == "a":
         allowed_ips = cluster_a_ips
     else:
         allowed_ips = cluster_b_ips
-
 
     start_server()
--- node.py
+++ node.py
@@ -5,12 +5,19 @@ import time  #  Added to keep script alive
 ip_address = socket.gethostbyname(socket.gethostname())
 
 def listen_for_messages():
-    """Node listens for incoming messages"""
+    
+    # Create client UDP socket
     client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
-    client.sendto(f"Heya from {container_name}".encode(), ((cluster_master), 9999))  
+    client.bind((ip_address, 9999))
 
-    data, addr = client.recvfrom(1024)
+    # Wait for msg 
+    data, addr = client.recvfrom(1024) 
+    print(data.decode())
+
+    client.sendto(f"Heya from {container_name}".encode(), addr)  
+
+    data, addr = client.recvfrom(1024)  # Wait for msg
     print(data.decode())
 
 
@@ -18,15 +25,9 @@ if __name__ == "__main__":
     container_name = os.getenv("container_name", "unknown")
     print(f"Wasgood I am {container_name}, ip addy= {ip_address}")
 
-    if str(ip_address)[-2] == "a":
-        cluster_master = "172.16.0.2"
+    if str(container_name)[-2] == "a":
+        cluster_master_ip = "172.16.0.2"
     else:
-        cluster_master = "172.16.0.10"
-
+        cluster_master_ip = "172.16.0.10"
 
     listen_for_messages()
-    # threading.Thread(target=listen_for_messages, daemon=True).start()
-
-    # # Keep the script running
-    # while True:
-    #     time.sleep(1)  # Prevents container from exiting
