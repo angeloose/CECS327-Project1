0fbe8729b1dea8ef31a3a4552fdf7a32fd9cb1ab
ea269 <emmanuelandrade189@gmail.com>
2025-03-15 00:17:43 -0700

    merged eandrade to main

--- client.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 9999))
-
-client.send("Hello From client".encode())
-print(client.recv(1024))
-
